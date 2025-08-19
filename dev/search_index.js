1aa16b0991af07b943ea5d53a26a5a00f689affe
Documenter.jl <documenter@juliadocs.github.io>
2025-08-19 19:19:38 +0000

    build based on 6a51f5f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RayTracingEngine","page":"Home","title":"RayTracingEngine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RayTracingEngine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
